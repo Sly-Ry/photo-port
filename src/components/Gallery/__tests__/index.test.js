8babc29a6e6cb2dad0815f56beb8fa99c9719a32
ryan <ryan.williams93@aol.com>
2022-03-24 19:17:22 -0500

    created gallery photolist tests

---
+++ src/components/Gallery/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '..';
+
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
+afterEach(cleanup);
+
+describe('Gallery is rendering', () => {
+
+    it('renders', () => {
+        render(<Gallery currentCategory={portrait} />);
+    });
+
+    // Test to make sure the gallery matches its snapshot
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Gallery  currentCategory={portrait} />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    // Test to make sure that the title of our <h1> is Portraits
+    it('renders', () => {
+        const { getByTestId } = render(<Gallery  currentCategory={portrait} />);
+        expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+    });
+});
